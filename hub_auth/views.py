50eb1ed4562f506b51f59407fd9ef79fe885621c
Omachonu Prosper <omachonucodes@gmail.com>
2025-05-12 05:17:27 +0100

    Create and configure a simple hello world endpoint

---
+++ hub_auth/views.py
@@ -0,0 +1,12 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HomeView(APIView):
+    def get(self, request):
+        """
+        This is a simple test view
+
+        It serves as a simple example of what subsequent views will look like
+        Custom classes for response and error handlilng will be introduced as we progress
+        """
+        return Response({'message': 'Hello World!'})
