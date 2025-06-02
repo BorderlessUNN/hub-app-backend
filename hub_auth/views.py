dd19142df1bf988c89857a8367bb095e132a5c3e
Franklyn-Ukwueze <franklynifechukwu2@gmail.com>
2025-06-02 23:01:29 +0100

    removed model imports from views

--- hub_auth/views.py
+++ hub_auth/views.py
@@ -1,8 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Member, Guest, Seat, Booking
-from .serializers import CheckInSerializer, GuestSerializer, SeatBookingSerializer
+
 
 class HomeView(APIView):
     def get(self, request):
