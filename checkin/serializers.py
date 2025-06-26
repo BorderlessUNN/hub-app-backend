2800c5846503fa8f5c5aa0121d38cc1eeb7d76b8
Franklyn-Ukwueze <franklynifechukwu2@gmail.com>
2025-06-26 12:05:49 +0100

    fixed bug in double check-in check and refactored member check-in serializer

--- checkin/serializers.py
+++ checkin/serializers.py
@@ -8,36 +8,44 @@ class MemberCheckInSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True)
 
     def validate(self, attrs):
-        email = attrs.get('email')
-        email = email.lower()
+        email = attrs.get('email').lower()
         try:
-            self.member = CustomUser.objects.get(email=email, is_member=True)
+            member = CustomUser.objects.get(email=email, is_member=True)
         except CustomUser.DoesNotExist:
             raise CustomValidationException(
-                    msg="No member found with this email",
-                    code=404
-                )
-        # Check if the user already checked in today
-        last_checkin = self.member.last_checkin
-        today = timezone.now().date()
+                msg="No member found with this email",
+                code=404
+            )
 
-        if last_checkin and last_checkin == today:
+        today = timezone.now().date()
+        if member.last_checkin and member.last_checkin.date() == today:
             raise CustomValidationException(
-                msg=f"{self.member.email} has already checked in today.",
+                msg=f"{member.email} has already checked in today.",
                 code=400
             )
-        # Update the last_checkin field
-        self.member.last_checkin = timezone.now()
+
+        self.member = member
+        return attrs
+
+    def save(self):
+        now = timezone.now()
+
+        self.member.last_checkin = now
         self.member.save(update_fields=["last_checkin"])
 
-        CheckIn.objects.create(user=self.member)
+        # Log Check-in
+        CheckIn.objects.create(user=self.member, timestamp=now)
 
+        return self.member
+
+    def to_representation(self):
         return {
             "id": str(self.member.id),
             "name": self.member.user_name,
             "email": self.member.email,
             "last_checkin": self.member.last_checkin
         }
+
 
 class GuestCheckInSerializer(serializers.Serializer):
     user_name = serializers.CharField(required=True, max_length=100)
--- checkin/views.py
+++ checkin/views.py
@@ -14,14 +14,19 @@ class MemberCheckInView(APIView):
     permission_classes = [IsAdminUser]
 
     def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        return CustomResponse(
+        serializer = MemberCheckInSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return CustomResponse(
             valid=True,
             status=status.HTTP_200_OK,
             msg="Check-in successful.",
-            data=serializer.validated_data
+            data=serializer.data
         )
+        return CustomResponse(
+            valid=False,
+            msg=serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST)
 
 class GuestCheckInView(APIView):
     """
