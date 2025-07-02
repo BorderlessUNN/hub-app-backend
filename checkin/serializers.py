d9890717273177a2f9d322d8e2493ac507d36b10
Franklyn-Ukwueze <franklynifechukwu2@gmail.com>
2025-07-02 03:17:52 +0100

    changed MemberCheckinSerializer to UserExistsSerializer and removed last_checkin

--- checkin/serializers.py
+++ checkin/serializers.py
@@ -3,7 +3,7 @@ from accounts.models import CustomUser
 from helpers.exceptions import CustomValidationException
 from django.utils import timezone
 
-class MemberCheckInSerializer(serializers.Serializer):
+class UserExistsSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True)
 
     def validate(self, attrs):
@@ -16,23 +16,8 @@ class MemberCheckInSerializer(serializers.Serializer):
                 code=404
             )
 
-        today = timezone.now().date()
-        if member.last_checkin and member.last_checkin.date() == today:
-            raise CustomValidationException(
-                msg=f"{member.email} has already checked in today.",
-                code=400
-            )
-
         self.member = member
         return attrs
-
-    def save(self):
-        now = timezone.now()
-
-        self.member.last_checkin = now
-        self.member.save(update_fields=["last_checkin"])
-
-        return self.member
 
     def to_representation(self):
         return {
@@ -40,5 +25,4 @@ class MemberCheckInSerializer(serializers.Serializer):
             "name": self.member.user_name,
             "email": self.member.email,
             "is_member": self.member.is_member,
-            "last_checkin": self.member.last_checkin
         }
