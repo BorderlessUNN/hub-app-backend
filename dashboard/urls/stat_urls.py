d6afe3f128b889653d12f6087b66dda47b989708
Franklyn-Ukwueze <franklynifechukwu2@gmail.com>
2025-07-04 03:55:42 +0100

    created path for UserStatsView

--- dashboard/urls/stat_urls.py
+++ dashboard/urls/stat_urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ( 
+from ..views import ( 
     CheckInStatsView,
+    UserStatsView
 )
 
 urlpatterns = [
     path('checkins/', CheckInStatsView.as_view(), name='check_in_stats'),
+    path('users/', UserStatsView.as_view(), name='user_stats'),
 ]
