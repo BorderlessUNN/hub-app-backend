d5f27591162add622394fa20bce9786983a7dafb
Franklyn-Ukwueze <franklynifechukwu2@gmail.com>
2025-07-04 01:14:25 +0100

    added dashboard app to settings file

--- hub_auth/settings.py
+++ hub_auth/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'seats.apps.SeatsConfig',
     'payments.apps.PaymentsConfig',
+    'dashboard.apps.DashboardConfig',
 ]
 
 MIDDLEWARE = [
